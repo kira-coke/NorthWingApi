2e73ef2d7908714d963356b0af160e43b00152bd
kira-coke <kiracoke@outlook.com>
2022-07-28 14:21:27 +0100

    Initial commit

---
+++ src/main/java/com/sparta/northwingapi/NorthWingApiApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.northwingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NorthWingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NorthWingApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/northwingapi/NorthWingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.northwingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NorthWingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
