895125ba576e30226c7559d13b27c6dc48625e8c
kira-coke <kiracoke@outlook.com>
2022-07-29 12:04:59 +0100

    All get mappings

---
+++ src/main/java/com/sparta/northwingapi/controllers/CustomerController.java
@@ -0,0 +1,81 @@
+package com.sparta.northwingapi.controllers;
+
+import com.sparta.northwingapi.entity.CustomerEntity;
+import com.sparta.northwingapi.repository.CustomerEntityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.persistence.FetchType;
+import javax.persistence.OneToOne;
+import java.util.List;
+
+@RestController
+public class CustomerController {
+    @Autowired
+    private CustomerEntityRepository repo;
+    @GetMapping("/customers")
+    public List<CustomerEntity> getCustomers(){
+        return repo.findAll();
+    }
+    @GetMapping("/customer/{id}")
+    public CustomerEntity getById(@PathVariable String id){
+        return repo.findById(id).get();
+    }
+    @GetMapping("/customer/company/{companyName}")
+    public List<CustomerEntity> getByCompanyName(@PathVariable String companyName){
+        List<CustomerEntity> result = repo.findCustomerEntitiesByCompanyName(companyName);
+        return result;
+    }
+    @GetMapping("/customer/contactName/{name}")
+    public CustomerEntity getByName(@PathVariable String name){
+        return repo.findCustomerEntityByContactName(name);
+    }
+    @GetMapping("/customer/postcode/{postcode}")
+    public List<CustomerEntity> getByPostalCode(@PathVariable String postcode){
+        return repo.findCustomerEntitiesByPostalCode(postcode);
+    }
+    @GetMapping("/customer/country/{country}")
+    public List<CustomerEntity> getByCountry(@PathVariable String country){
+        return repo.findCustomerEntitiesByCountry(country);
+    }
+    @GetMapping("/customer/city/{city}")
+    public List<CustomerEntity> getByCity(@PathVariable String city){
+        return repo.findCustomerEntitiesByCity(city);
+    }
+    @GetMapping("/customer/address/{address}")
+    public CustomerEntity getByAddress(@PathVariable String address){
+        return repo.findCustomerEntityByAddress(address);
+    }
+    @GetMapping("/customer/contactTitle/{contactTitle}")
+    public List<CustomerEntity> getByContactTitle(@PathVariable String contactTitle){
+        return repo.findCustomerEntitiesByContactTitle(contactTitle);
+    }
+    @GetMapping("/customer/region/{region}")
+    public List<CustomerEntity> getByRegion(@PathVariable String region){
+        return repo.findCustomerEntitiesByRegion(region);
+    }
+    @GetMapping("/customer/phone/{phone}")
+    public CustomerEntity getByPhone(@PathVariable String phone){
+        return repo.findCustomerEntityByPhone(phone);
+    }
+    @GetMapping("/customer/fax/{fax}")
+    public CustomerEntity getByFax(@PathVariable String fax){
+        return repo.findCustomerEntityByFax(fax);
+    }
+    @GetMapping("/customer/{country}/{contactTitle}")
+    public List<CustomerEntity> getByCountryAndContactTitle(@PathVariable String country, @PathVariable String contactTitle){
+        return repo.findCustomerEntitiesByContactTitleAndCountry(contactTitle, country);
+    }
+
+    /*@DeleteMapping("/customer/{id}/delete")
+    @OneToOne(fetch = FetchType.LAZY)
+    public void deleteById(@PathVariable String id){
+        //CustomerEntity result = repo.findById(id).get();
+        if(repo.existsById(id)){
+            repo.deleteById(id);
+        }else{
+            throw new RuntimeException("No record found for given id: "+id);
+        }
+    }*/
+
+}
--- src/main/java/com/sparta/northwingapi/repository/CustomerEntityRepository.java
+++ src/main/java/com/sparta/northwingapi/repository/CustomerEntityRepository.java
@@ -3,5 +3,28 @@ package com.sparta.northwingapi.repository;
 import com.sparta.northwingapi.entity.CustomerEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CustomerEntityRepository extends JpaRepository<CustomerEntity, String> {
+    List<CustomerEntity> findCustomerEntitiesByCompanyName(String companyName);
+
+    CustomerEntity findCustomerEntityByContactName(String contantName);
+
+    List<CustomerEntity> findCustomerEntitiesByPostalCode(String postCode);
+
+    List<CustomerEntity> findCustomerEntitiesByCountry(String country);
+
+    List<CustomerEntity> findCustomerEntitiesByCity(String city);
+
+    CustomerEntity findCustomerEntityByAddress(String address);
+
+    List<CustomerEntity> findCustomerEntitiesByContactTitle(String contactTitle);
+
+    List<CustomerEntity> findCustomerEntitiesByRegion(String region);
+    CustomerEntity findCustomerEntityByPhone(String phone);
+    CustomerEntity findCustomerEntityByFax(String fax);
+
+    List<CustomerEntity> findCustomerEntitiesByContactTitleAndCountry(String title, String country);
+
+
 }
